2a15c81fad031648e029fabd0f77d13573a8e34a
Alexis Varela <alexisbec13@hotmail.com>
2021-02-15 14:00:45 -0600

    updated excercise.sql file

--- excercise.sql
+++ excercise.sql
@@ -1,118 +1,118 @@
-SELECT population FROM world
+SELECT populatiON FROM wORld
   WHERE name = 'Germany'
 
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark')
+SELECT name, populatiON FROM wORld
+  WHERE name IN ('Sweden', 'NORway', 'Denmark')
 
-SELECT name, area FROM world
+SELECT name, area FROM wORld
   WHERE area BETWEEN 200000 AND 250000
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE 'Y%'
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE '%y'
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE '%x%'
 
-SELECT name FROM world
-  WHERE name LIKE '%land'
+SELECT name FROM wORld
+  WHERE name LIKE '%lAND'
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE 'C%ia'
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE '%oo%'
 
-SELECT name FROM world
+SELECT name FROM wORld
   WHERE name LIKE '%a%a%a%'
 
-SELECT name FROM world
+SELECT name FROM wORld
  WHERE name LIKE '_t%'
 ORDER BY name
 
-SELECT name FROM world
+SELECT name FROM wORld
  WHERE name LIKE '%o__o%'
 
-SELECT name FROM world
+SELECT name FROM wORld
  WHERE name LIKE '____'
 
 SELECT name
-  FROM world
+  FROM wORld
  WHERE name LIKE capital
 
 SELECT name
-  FROM world
- WHERE capital LIKE concat(name,' city')
+  FROM wORld
+ WHERE capital LIKE cONcat(name,' city')
 
-Select capital, name
-From world
-where capital like concat(name,'%')
+SELECT capital, name
+FROM wORld
+WHERE capital LIKE cONcat(name,'%')
 
-select capital, name
-from world
-where capital like concat(name,'_%')
+SELECT capital, name
+FROM wORld
+WHERE capital LIKE cONcat(name,'_%')
 
-select name, replace(capital, name, '')
-from world
-where capital like concat(name,'_%')
+SELECT name, replace(capital, name, '')
+FROM wORld
+WHERE capital LIKE cONcat(name,'_%')
 
-SELECT name, continent, population FROM world
+SELECT name, cONtinent, populatiON FROM wORld
 
-SELECT name FROM world
-WHERE population >= 200000000
+SELECT name FROM wORld
+WHERE populatiON >= 200000000
 
-SELECT name, (gdp / population) as per_capita_gdp FROM world
-WHERE population >= 200000000
+SELECT name, (gdp / populatiON) AS per_capita_gdp FROM wORld
+WHERE populatiON >= 200000000
 
-select name, (population / 1000000) population_in_millions
-from world
-where continent = 'South America'
+SELECT name, (populatiON / 1000000) populatiON_in_milliONs
+FROM wORld
+WHERE cONtinent = 'South America'
 
-select name, population
-from world
-where name in('France', 'Germany', 'Italy')
+SELECT name, populatiON
+FROM wORld
+WHERE name in('France', 'Germany', 'Italy')
 
-select name
-from world
-where name like '%United%'
+SELECT name
+FROM wORld
+WHERE name LIKE '%United%'
 
-select name, population, area
-from world
-where area >= 3000000
-or population >= 250000000
+SELECT name, populatiON, area
+FROM wORld
+WHERE area >= 3000000
+OR populatiON >= 250000000
 
-select name, population, area
-from world
-where area >= 3000000
-xor population >= 250000000
+SELECT name, populatiON, area
+FROM wORld
+WHERE area >= 3000000
+xOR populatiON >= 250000000
 
-select name, round(population/1000000, 2), round(gdp/1000000000, 2)
-from world
-where continent= 'South America'
+SELECT name, round(populatiON/1000000, 2), round(gdp/1000000000, 2)
+FROM wORld
+WHERE cONtinent= 'South America'
 
-select name, round(gdp / population, -3)
-from world
-where gdp >= 1000000000000
+SELECT name, round(gdp / populatiON, -3)
+FROM wORld
+WHERE gdp >= 1000000000000
 
 SELECT name, capital
-  FROM world
+  FROM wORld
  WHERE LENGTH(name) = LENGTH(capital)
 
 SELECT name, capital
-FROM world
-where left(name, 1) = left(capital, 1)
-and name != capital
+FROM wORld
+WHERE LEFT(name, 1) = LEFT(capital, 1)
+AND name != capital
 
 SELECT name
-   FROM world
-WHERE name not like '% %'
-and name like '%a%'
-and name like '%e%'
-and name like '%i%'
-and name like '%o%'
-and name like '%u%'
+   FROM wORld
+WHERE name not LIKE '% %'
+AND name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
 
 SELECT yr, subject, winner
   FROM nobel
@@ -123,336 +123,336 @@ SELECT winner
  WHERE yr = 1962
    AND subject = 'Literature'
 
-select yr, subject
-from nobel
-where winner = 'Albert Einstein'
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
 
-select winner
-from nobel
-where subject = 'Peace'
-and yr >= 2000
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace'
+AND yr >= 2000
 
-select yr, subject, winner
-from nobel
-where subject = 'Literature'
-and yr between 1980 and 1989
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature'
+AND yr between 1980 AND 1989
 
 SELECT * FROM nobel
- WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+ WHERE winner IN ('TheodORe Roosevelt', 'Woodrow WilsON', 'Jimmy Carter', 'Barack Obama')
 
-select winner from nobel
-where winner like 'John %'
+SELECT winner FROM nobel
+WHERE winner LIKE 'John %'
 
-select yr, subject, winner from nobel
-where (yr = 1980 and subject = 'Physics')
-or (yr = 1984 and subject = 'Chemistry')
+SELECT yr, subject, winner FROM nobel
+WHERE (yr = 1980 AND subject = 'Physics')
+OR (yr = 1984 AND subject = 'Chemistry')
 
-select yr, subject, winner from nobel
-where yr = 1980
-and subject not in ('Chemistry', 'Medicine')
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980
+AND subject not in ('Chemistry', 'Medicine')
 
-select yr, subject, winner from nobel
-where (subject = 'Medicine' and yr < 1910)
-or (subject = 'Literature' and yr >= 2004)
+SELECT yr, subject, winner FROM nobel
+WHERE (subject = 'Medicine' AND yr < 1910)
+OR (subject = 'Literature' AND yr >= 2004)
 
-select * from nobel
-where winner = 'PETER GRÜNBERG'
+SELECT * FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
 
-select * from nobel
-where winner = "EUGENE O\'NEILL"
+SELECT * FROM nobel
+WHERE winner = "EUGENE O\'NEILL"
 
-select winner, yr, subject from nobel
-where winner like 'Sir%'
-order by yr desc,winner
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr desc,winner
 
 SELECT winner, subject
   FROM nobel
  WHERE yr=1984
  ORDER BY subject IN ('Physics','Chemistry'),subject,winner
 
-SELECT name FROM world
-  WHERE population >
-     (SELECT population FROM world
+SELECT name FROM wORld
+  WHERE populatiON >
+     (SELECT populatiON FROM wORld
       WHERE name='Russia')
 
-select name
-from world
-where continent = 'Europe' and
-gdp/population > ( select gdp/population from world where name = 'United Kingdom' )
+SELECT name
+FROM wORld
+WHERE cONtinent = 'Europe' AND
+gdp/populatiON > ( select gdp/populatiON FROM wORld WHERE name = 'United Kingdom' )
 
-select name, continent from world
-where continent = (select continent from world where name = 'Argentina')
-or continent = (select continent from world where name = 'Australia')
-order by name
+SELECT name, cONtinent FROM wORld
+WHERE cONtinent = (SELECT cONtinent FROM wORld WHERE name = 'Argentina')
+OR cONtinent = (SELECT cONtinent FROM wORld WHERE name = 'Australia')
+ORDER BY name
 
-select name, population from world
-where population > (select population from world where name = 'Canada')
-and population < (select population from world where name = 'Polonia')
+SELECT name, populatiON FROM wORld
+WHERE populatiON > (SELECT populatiON FROM wORld WHERE name = 'Canada')
+AND populatiON < (SELECT populatiON FROM wORld WHERE name = 'PolONia')
 
-select name, 
-concat(round(100*population/(select population from world where name = 'Germany')),'%')
-from world
-where continent = 'Europe'
+SELECT name, 
+cONcat(round(100*populatiON/(select populatiON FROM wORld WHERE name = 'Germany')),'%')
+FROM wORld
+WHERE cONtinent = 'Europe'
 
-select name from world 
-where gdp > (select max(gdp) from world where continent = 'Europe')
+SELECT name FROM wORld 
+WHERE gdp > (select max(gdp) FROM wORld WHERE cONtinent = 'Europe')
 
-SELECT continent, name, area FROM world x
+SELECT cONtinent, name, area FROM wORld x
   WHERE area >= ALL
-    (SELECT area FROM world y
-        WHERE y.continent=x.continent
+    (SELECT area FROM wORld y
+        WHERE y.cONtinent=x.cONtinent
           AND area>0)
 
-select continent, min(name) as name from world
-group by continent
-order by continent
+SELECT cONtinent, min(name) AS name FROM wORld
+GROUP BY cONtinent
+ORDER BY cONtinent
 
-select continent, sum(population) as pop from world a
-group by continent
+SELECT cONtinent, sum(populatiON) AS pop FROM wORld a
+GROUP BY cONtinent
 
-SELECT SUM(population)
-FROM world
+SELECT SUM(populatiON)
+FROM wORld
 
-select continent from world
-group by continent
+SELECT cONtinent FROM wORld
+GROUP BY cONtinent
 
-select sum(gdp) from world
-where continent = 'Africa'
+SELECT sum(gdp) FROM wORld
+WHERE cONtinent = 'Africa'
 
-select count(name) from world
-where area >= 1000000
+SELECT count(name) FROM wORld
+WHERE area >= 1000000
 
-select sum(population) from world
-where name in('Estonia', 'Latvia', 'Lithuania')
+SELECT sum(populatiON) FROM wORld
+WHERE name in('EstONia', 'Latvia', 'Lithuania')
 
-select continent, count(name) from world
-group by continent
+SELECT cONtinent, count(name) FROM wORld
+GROUP BY cONtinent
 
-select continent, count(name) from world
-where population >= 10000000
-group by continent
+SELECT cONtinent, count(name) FROM wORld
+WHERE populatiON >= 10000000
+GROUP BY cONtinent
 
-select continent from world a
-where 
-(select sum(population) from world where continent = a.continent group by continent) >= 100000000
-group by continent
+SELECT cONtinent FROM wORld a
+WHERE 
+(SELECT sum(populatiON) FROM wORld WHERE cONtinent = a.cONtinent GROUP BY cONtinent) >= 100000000
+GROUP BY cONtinent
 
 SELECT matchid, player FROM goal 
   WHERE teamid = 'GER'
 
 SELECT id,stadium,team1,team2
   FROM game
-where id = 1012
+WHERE id = 1012
 
 SELECT player, teamid, stadium, mdate
   FROM game JOIN goal ON (id=matchid)
-where goal.teamid = 'GER'
+WHERE goal.teamid = 'GER'
 
-select team1, team2, player from game
-join goal on id=matchid
-where player like 'Mario%'
+SELECT team1, team2, player FROM game
+JOIN goal ON id=matchid
+WHERE player LIKE 'Mario%'
 
 SELECT player, teamid, coach, gtime
-  FROM goal join eteam on teamid=id
+  FROM goal JOIN eteam ON teamid=id
  WHERE gtime<=10
 
-select mdate, teamname from game
-join eteam on team1=eteam.id
-where coach = 'Fernando Santos'
+SELECT mdate, teamname FROM game
+JOIN eteam ON team1=eteam.id
+WHERE coach = 'FernANDo Santos'
 
-select player from goal
-join game on matchid=id
-where stadium = 'National Stadium, Warsaw'
+SELECT player FROM goal
+JOIN game ON matchid=id
+WHERE stadium = 'NatiONal Stadium, Warsaw'
 
 SELECT distinct(player)
   FROM game JOIN goal ON matchid = id 
-    WHERE (team1='GER' or team2='GER')
-and teamid != 'GER'
+    WHERE (team1='GER' OR team2='GER')
+AND teamid != 'GER'
 
 SELECT teamname, count(gtime)
   FROM eteam JOIN goal ON id=teamid
-group by teamname
+GROUP BY teamname
  ORDER BY teamname
 
-select stadium, count(gtime) from game
-join goal on id=matchid
-group by stadium
+select stadium, count(gtime) FROM game
+JOIN goal ON id=matchid
+GROUP BY stadium
 
 SELECT matchid,mdate, count(gtime)
   FROM game JOIN goal ON matchid = id 
  WHERE (team1 = 'POL' OR team2 = 'POL')
-group by matchid, mdate
+GROUP BY matchid, mdate
 
 SELECT matchid,mdate, count(gtime)
   FROM game JOIN goal ON matchid = id 
  WHERE (team1 = 'GER' OR team2 = 'GER')
-and teamid = 'GER'
-group by matchid, mdate
+AND teamid = 'GER'
+GROUP BY matchid, mdate
 
 SELECT id,mdate,
   team1,
   CASE WHEN teamid=team1 THEN 
-  (select count(gtime) from goal where teamid = team1 and matchid = id) else 0
-  END score1,
+  (SELECT count(gtime) FROM goal WHERE teamid = team1 AND matchid = id) ELSE 0
+  END scORe1,
   team2,
-  case when teamid=team2 then 
-  (select count(gtime) from goal where teamid = team2 and matchid = id) else 0
-  end score2
+  CASE when teamid=team2 then 
+  (SELECT count(gtime) FROM goal WHERE teamid = team2 AND matchid = id) ELSE 0
+  end scORe2
   FROM game x JOIN goal ON matchid = id
-group by id, mdate, team1, teamid, team2
-order by mdate
+GROUP BY id, mdate, team1, teamid, team2
+ORDER BY mdate
 
 SELECT id, title
  FROM movie
  WHERE yr=1962
 
-select yr from movie
-where title = 'Citizen Kane'
+SELECT yr FROM movie
+WHERE title = 'Citizen Kane'
 
-select id, title, yr from movie
-where title like '%Star Trek%'
-order by yr
+SELECT id, title, yr FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr
 
-select id from actor
-where name = 'Glenn Close'
+SELECT id FROM actOR
+WHERE name = 'Glenn Close'
 
-select id from movie
-where title = 'Casablanca'
+SELECT id FROM movie
+WHERE title = 'CASablanca'
 
-select name from movie a
-join casting b on a.id=b.movieid
-join actor c on b.actorid=c.id
-where a.id=11768
+SELECT name FROM movie a
+JOIN cASting b ON a.id=b.movieid
+JOIN actOR c ON b.actORid=c.id
+WHERE a.id=11768
 
-select name from movie a
-join casting b on a.id=b.movieid
-join actor c on b.actorid=c.id
-where a.title = 'Alien'
+SELECT name FROM movie a
+JOIN cASting b ON a.id=b.movieid
+JOIN actOR c ON b.actORid=c.id
+WHERE a.title = 'Alien'
 
-select title from movie a
-join casting b on a.id=b.movieid
-join actor c on b.actorid=c.id
-where c.name = 'Harrison Ford'
+SELECT title FROM movie a
+JOIN cASting b ON a.id=b.movieid
+JOIN actOR c ON b.actORid=c.id
+WHERE c.name = 'HarrisON FORd'
 
-select title from movie a
-join casting b on a.id=b.movieid
-join actor c on b.actorid=c.id
-where c.name = 'Harrison Ford'
-and b.ord !=1
+SELECT title FROM movie a
+JOIN cASting b ON a.id=b.movieid
+JOIN actOR c ON b.actORid=c.id
+WHERE c.name = 'HarrisON FORd'
+AND b.ORd !=1
 
-select a.title, c.name from movie a
-join casting b on a.id=b.movieid
-join actor c on b.actorid=c.id
-where b.ord = 1
+SELECT a.title, c.name FROM movie a
+JOIN cASting b ON a.id=b.movieid
+JOIN actOR c ON b.actORid=c.id
+WHERE b.ORd = 1
 
 SELECT yr,COUNT(title) FROM
-  movie JOIN casting ON movie.id=movieid
-        JOIN actor   ON actorid=actor.id
-WHERE name='Rock Hudson'
+  movie JOIN cASting ON movie.id=movieid
+        JOIN actOR   ON actORid=actOR.id
+WHERE name='Rock HudsON'
 GROUP BY yr
 HAVING COUNT(title) > 2
 
-select name from teacher
-where dept is null
+SELECT name FROM teacher
+WHERE dept is null
 
 SELECT teacher.name, dept.name
  FROM teacher JOIN dept
            ON (teacher.dept=dept.id)
 
 SELECT teacher.name, dept.name
- FROM teacher left JOIN dept
+ FROM teacher LEFT JOIN dept
            ON (teacher.dept=dept.id)
 
 SELECT teacher.name, dept.name
- FROM teacher right JOIN dept
+ FROM teacher RIGHT JOIN dept
            ON (teacher.dept=dept.id)
 
-select name, COALESCE(mobile, '07986 444 2266') from teacher
+SELECT name, COALESCE(mobile, '07986 444 2266') FROM teacher
 
 SELECT A_STRONGLY_AGREE
   FROM nss
- WHERE question='Q01'
-   AND institution='Edinburgh Napier University'
+ WHERE questiON='Q01'
+   AND institutiON='Edinburgh Napier University'
    AND subject='(8) Computer Science'
 
-SELECT institution, subject
+SELECT institutiON, subject
   FROM nss
- WHERE question='Q15'
-   AND score>=100
+ WHERE questiON='Q15'
+   AND scORe>=100
 
-SELECT institution,score
+SELECT institutiON,scORe
   FROM nss
- WHERE question='Q15'
-   AND score<50
+ WHERE questiON='Q15'
+   AND scORe<50
    AND subject='(8) Computer Science'
 
-SELECT subject, sum(response)
+SELECT subject, sum(respONse)
   FROM nss
- WHERE question='Q22'
-   AND (subject='(8) Computer Science' or subject='(H) Creative Arts and Design')
-group by subject
+ WHERE questiON='Q22'
+   AND (subject='(8) Computer Science' OR subject='(H) Creative Arts AND Design')
+GROUP BY subject
 
-SELECT subject, sum(response*A_STRONGLY_AGREE*.01) 
+SELECT subject, sum(respONse*A_STRONGLY_AGREE*.01) 
   FROM nss
- WHERE question='Q22'
-   AND (subject='(8) Computer Science' or subject='(H) Creative Arts and Design')
-group by subject
+ WHERE questiON='Q22'
+   AND (subject='(8) Computer Science' OR subject='(H) Creative Arts AND Design')
+GROUP BY subject
 
-SELECT subject, round(sum(response*A_STRONGLY_AGREE/100)/sum(response)*100,0)
+SELECT subject, round(sum(respONse*A_STRONGLY_AGREE/100)/sum(respONse)*100,0)
   FROM nss
- WHERE question='Q22'
-   AND (subject='(8) Computer Science' or subject='(H) Creative Arts and Design')
-group by subject
+ WHERE questiON='Q22'
+   AND (subject='(8) Computer Science' OR subject='(H) Creative Arts AND Design')
+GROUP BY subject
 
-SELECT institution, ROUND(SUM(response*score/100)/SUM(response)*100,0)
+SELECT institutiON, ROUND(SUM(respONse*scORe/100)/SUM(respONse)*100,0)
   FROM nss
- WHERE question='Q22'
-   AND (institution LIKE '%Manchester%')
-group by institution
-ORDER BY institution
+ WHERE questiON='Q22'
+   AND (institutiON LIKE '%Manchester%')
+GROUP BY institutiON
+ORDER BY institutiON
 
-SELECT institution, sum(sample),
+SELECT institutiON, sum(sample),
 (SELECT sample FROM nss y
 WHERE subject='(8) Computer Science'
-AND x.institution = y.institution
-AND question='Q01')
+AND x.institutiON = y.institutiON
+AND questiON='Q01')
   FROM nss x
- WHERE question='Q01'
-   AND (institution LIKE '%Manchester%')
-group by institution
+ WHERE questiON='Q01'
+   AND (institutiON LIKE '%Manchester%')
+GROUP BY institutiON
 
-SELECT lastName, party, votes
+SELECT lAStName, party, votes
   FROM ge
- WHERE constituency = 'S14000024' AND yr = 2017
+ WHERE cONstituency = 'S14000024' AND yr = 2017
 ORDER BY votes DESC
 
 SELECT party, votes,
-       RANK() OVER (ORDER BY votes DESC) as posn
+       RANK() OVER (ORDER BY votes DESC) AS posn
   FROM ge
- WHERE constituency = 'S14000024' AND yr = 2017
+ WHERE cONstituency = 'S14000024' AND yr = 2017
 ORDER BY party
 
 SELECT yr,party, votes,
-      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) AS posn
   FROM ge
- WHERE constituency = 'S14000021'
+ WHERE cONstituency = 'S14000021'
 ORDER BY party,yr
 
-SELECT constituency, party
+SELECT cONstituency, party
 FROM ge x 
-WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
-AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
-ORDER BY constituency,votes DESC
+WHERE cONstituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.cONstituency = y. cONstituency AND y.yr = 2017)
+ORDER BY cONstituency,votes DESC
 
 SELECT party , COUNT(*)
 FROM ge x 
-WHERE constituency like 'S%'
-AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+WHERE cONstituency LIKE 'S%'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.cONstituency = y. cONstituency AND y.yr = 2017)
 GROUP BY party
 
-select count(id) from stops
+SELECT count(id) FROM stops
 
-select id from stops where name = 'Craiglockhart'
+SELECT id FROM stops WHERE name = 'Craiglockhart'
 
 SELECT id, name FROM stops, route
 WHERE id=stop
